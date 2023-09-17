c7c209e5c6055331d39f33de9a1d1c244a9fcbd7
orange <github@sane-excel.com>
2023-09-17 18:03:38 +0900

    20230917-01

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {reactive} from "vue";
+
+const data = reactive({
+  message:'',
+})
+
+data.message = 'Hello, Vue3!!!'
+</script>
+
+<template>
+  <div>{{data.message}}</div>
+</template>
+
+<style scoped>
+</style>
